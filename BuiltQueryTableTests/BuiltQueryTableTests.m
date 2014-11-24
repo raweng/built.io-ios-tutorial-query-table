7047be8725568c552de24dbd93eadff7f17efdb3
Reefaq <reefaq@Reefaqs-Mac-mini.local>
2014-11-24 14:34:03 +0530

    updated sdk

--- BuiltQueryTableTests/BuiltQueryTableTests.m
+++ BuiltQueryTableTests/BuiltQueryTableTests.m
@@ -2,10 +2,11 @@
 //  BuiltQueryTableTests.m
 //  BuiltQueryTableTests
 //
-//  Created by Sameer on 03/03/14.
+//  Created by Reefaq on 24/11/14.
 //  Copyright (c) 2014 Sameer. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
 @interface BuiltQueryTableTests : XCTestCase
@@ -14,21 +15,26 @@
 
 @implementation BuiltQueryTableTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
 }
 
 @end
